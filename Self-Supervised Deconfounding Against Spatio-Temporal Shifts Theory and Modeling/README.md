59e5559a8a0fd857bd1005b2affc9280bddca453
DingYX0731 <yixuan0248@gmail.com>
2024-04-14 15:12:54 +0800

    ablation and readme modified

--- Self-Supervised Deconfounding Against Spatio-Temporal Shifts Theory and Modeling/README.md
+++ Self-Supervised Deconfounding Against Spatio-Temporal Shifts Theory and Modeling/README.md
@@ -1,6 +1,6 @@
 # Reproduction
 
-## Self-Supervised Decoufounding Against Spatio-Temporal Shifts: Theory and Modeling
+## Self-Supervised Deconfounding Against Spatio-Temporal Shifts: Theory and Modeling
 
 ### Introduction
 
@@ -11,31 +11,97 @@ The following content aims for reproducing the paper:
 The process of reproduction includes:
 
 * **Data Preprocessing**: Since the data provided by the original repository is not enough for reproducing the OOD case in different scenario, this repository provides a data preprocess script for generating data needed.
+* **OOD evaluation**: spatial OOD evaluation and temporal OOD evaluation are conducted to discover the model's generalization ability.
 * **Ablation Study**: Six variants of ablation study mentioned in the paper is conducted.
  
 ### Quick Start
 
+*Note*: all the commands are run under `STEVE` directory.
+
 #### Data Preprocessing
 
-For data preprocessing, simply run the script:
+For data preprocessing, download the raw data then simply run the script:
 ```
 python data_preprocess.py
 ```
-*Input*: raw `NYCBike1` data file, which could be downloaded from [here](https://github.com/topazape/ST-ResNet/tree/main/datasets/BikeNYC)
+*Input*: raw `NYCBike1` data file, which could be downloaded from [here](https://github.com/topazape/ST-ResNet/tree/main/datasets/BikeNYC). For alignment with the script, just place the data under `STEVE` directory.
 
 *Output*:
 ```
 |----NYCBike1\
-|   |----train.npz          # training data
-|   |----adj_mx.npz         # predefined graph adjacency matrix
-|   |----total_test.npz     # test data (total)
-|   |----workday_test.npz   # test data (workday)
-|   |----holiday_test.npz   # test data (holiday)
-|   |----val.npz            # validation data
+|   |----train.npz                  # training data
+|   |----val.npz                    # validation data
+|   |----test.npz                   # test data
+|   |----total_test_dataloader.pkl  # test dataloader (total)
+|   |----workday_test_dataloder.pkl # temporal OOD data (workday)
+|   |----holiday_test.pkl           # temporal OOD data (holiday)
+|   |----cluste_labels.npy          # recording the spatial clusters for spatial OOD forecasting
+
 ```
 For each of the `npz` file, several components are included:
 
-* `x`: input data, which is a 4D tensor which indicates: `sample size`, `window size`, `num of nodes`, `flow features`.
-* `y`: output data, which is a 4D tensor which indicates: `sample size`, `predicted length`, `num of nodes`, `flow features`.
-* `time_label`: indicating the temporal type for corresponding output data. The label is ranging from 0 to 47, where 0-23 indicates hours in workdays, whereas 24-47 indicates hours in holidays
+* **x**: input data, which is a 4D tensor which indicates: `sample size`, `window size`, `num of nodes`, `flow features`.
+* **y**: output data, which is a 4D tensor which indicates: `sample size`, `predicted length`, `num of nodes`, `flow features`.
+* **time_label**: indicating the temporal type for corresponding output data. The label is ranging from 0 to 47, where 0-23 indicates hours in workdays, whereas 24-47 indicates hours in holidays.
+* **c**: indicating the traffic load capacity in self-supervised task 3 in the paper, having the same dimension with `y`.
 
+For each of the `pkl` file, two components are included:
+
+* `dataloader`: used in test phase for generating different types of OOD data.
+* `scaler`: used in test phase for data inverse transformation. 
+
+#### OOD evaluation:
+After doing data preprocess, data needed for temporal OOD forecasting and spatial OOD forecasting is obatained. Before evaluation, please train the whole model with full training data. Just set the configuration of `mode` as `train` and other modules like `cd`, `gr`, `sl`, `ti`, `tl` to be `true` from `configs/NYCBike1.yaml`, then simply run the script:
+```
+python run.py
+```
+After training, just easily configure `mode` as `test` and run the above script again, the output is saved in `experiments/NYCBike1/results.npz` file, which includes:
+* **Temporal OOD Forecasting**: prediction and ground truth of the workday data and holiday data.
+* **Spatial OOD Forecasting**: prediction and ground truth of respective spatial clusters data.
+* **OOD Evaluation Result**: `mae` and `mape` of all the aspects mentioned above.
+
+#### Ablation Study:
+When doing ablation study, please configure the module needed to be verified in configuration file `configs/NYCBike1.yaml`. Every configuration is a boolean type with `True` for implementation and `False` for ignorant.
+
+There are six variants of ablation study mentioned in the paper:
+* **cd**: Contextual Disentanglement Module
+* **gr**: Gradient Reversal Layer Implementation
+* **idp**: Independent Requirement of DCA
+* **sl**: Spatial Location Classification Self-supervised Task
+* **ti**: Temporal Index Identification Self-supervised Task
+* **tl**: Traffic Load Prediction Self-supervised Task
+
+When doing ablation study for a specific variant, just set the corresponding configuration as `False` and train the model. Note that when doing `idp`, you need to set both `cd` and `gr` as `False`.
+
+### Result
+
+#### OOD Evaluation
+* Temporal OOD Forecasting
+
+    | Metric | Workday | Holiday | Total |
+    |--------|---------|---------|-------|
+    | MAE    |  Value  |  Value  | Value |
+    | MAPE   |  Value  |  Value  | Value |
+
+* Spatial OOD Forecasting
+
+    | Metric | c0 | c1 | c2 | Total |
+    |--------|---------|---------|-------|------|
+    | MAE    |  Value  |  Value  | Value | Value |
+    | MAPE   |  Value  |  Value  | Value | Value |
+
+#### Ablation Study
+* Temporal OOD Forecasting
+
+    | Metric | STEVE | *w/o cd* | *w/o gr* | *w/o idp* | *w/o sl* | *w/0 ti* | *w/o tl*|
+    |--------|---------|---------|---------|-----------|----------|----------|---------|
+    | MAE    |  Value  |  Value  | Value |Value |Value |Value |Value |
+    | MAPE   |  Value  |  Value  | Value |Value |Value |Value |Value |
+
+
+* Spatial OOD Forecasting
+
+    | Metric | STEVE | *w/o cd* | *w/o gr* | *w/o idp* | *w/o sl* | *w/0 ti* | *w/o tl*|
+    |--------|---------|---------|---------|-----------|----------|----------|---------|
+    | MAE    |  Value  |  Value  | Value |Value |Value |Value |Value |
+    | MAPE   |  Value  |  Value  | Value |Value |Value |Value |Value |
---
+++ Self-Supervised Deconfounding Against Spatio-Temporal Shifts Theory and Modeling/STEVE/data_preprocess.py
@@ -0,0 +1,247 @@
+import os
+import sys
+import h5py
+import math
+import torch
+import pickle
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+from sklearn.preprocessing import StandardScaler
+from lib.dataloader import normalize_data
+
+def create_folder (folder_name):
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+        print(f"Folder {folder_name} created")
+    else:
+        print(f"Folder {folder_name} already exists")
+
+def split_x_y_tensor (tensor):
+    x_tensor = None
+    y_tensor = None
+    for i in range(tensor.size(0)-77):
+        window_list = [
+            tensor[i:i+5],
+            tensor[i+24:i+29],
+            tensor[i+48:i+53],
+            tensor[i+72:i+76]
+        ]
+        if x_tensor is None:
+            x_tensor = torch.cat(window_list, dim=0)
+            x_tensor = x_tensor.unsqueeze(0)
+        else:
+            window_list = torch.cat(window_list, dim=0).unsqueeze(0)
+            x_tensor = torch.cat((x_tensor, window_list), dim=0)
+
+        if y_tensor is None:
+            y_tensor = tensor[i+77].unsqueeze(0)
+        else:
+            y_tensor = torch.cat((y_tensor, tensor[i+77].unsqueeze(0)), dim=0)
+
+    x_tensor = x_tensor.permute(0, 1, 3, 2)
+    y_tensor = y_tensor.permute(0, 2, 1).unsqueeze(1)
+    return x_tensor, y_tensor
+
+
+def dayhour_to_timelabel(day, hour):
+    if day < 5: # workday
+        time_label = hour
+    else: # holiday
+        time_label = hour + 24
+    return time_label
+
+def move_forward_1h (hour, day):
+    hour += 1
+    if hour == 24:
+        day += 1
+        hour = 0
+        if day == 7:
+            day = 0
+    return hour, day
+
+def create_time_label (hour, day, size):
+    time_label = torch.zeros(size)
+    for i in range(time_label.size(0)):
+        time_label[i] = dayhour_to_timelabel(day, hour)
+        hour, day = move_forward_1h(hour, day)
+    return time_label
+
+def create_traffic_load (y_tensor):
+    max_CP_inflow = {}
+    max_CP_outflow = {}
+    c = torch.empty_like(y_tensor)
+
+    for i in range(y_tensor.size(0)):
+        for node in range(y_tensor.size(2)):
+            inflow = y_tensor[i][0][node][0]
+            outflow = y_tensor[i][0][node][1]
+            if node not in max_CP_inflow:
+                max_CP_inflow[node] = inflow
+            if node not in max_CP_outflow:
+                max_CP_outflow[node] = outflow
+            if inflow > max_CP_inflow[node]:
+                max_CP_inflow[node] = inflow
+            if outflow > max_CP_outflow[node]:
+                max_CP_outflow[node] = outflow
+            if max_CP_inflow[node] == 0:
+                c[i][0][node][0] = 0
+            else:
+                c[i][0][node][0] = math.ceil(5 * inflow / max_CP_inflow[node])
+            if max_CP_outflow[node] == 0:
+                c[i][0][node][1] = 0
+            else:
+                c[i][0][node][1] = math.ceil(5 * outflow / max_CP_outflow[node])
+    return c
+
+def split_train_val_test_sets (tensor):
+    # shuffle the data
+    torch.manual_seed(0)
+    indices = torch.randperm(x_tensor.size(0))
+    tensor = tensor[indices]
+
+    # split the data
+    train_size = int(0.7 * tensor.size(0))
+    val_size = int(0.2 * tensor.size(0))
+
+    tensor_train = tensor[:train_size]
+
+    tensor_val = tensor[train_size:train_size+val_size]
+
+    tensor_test = tensor[train_size+val_size:]
+
+    return tensor_train, tensor_val, tensor_test
+
+def create_cluster_labels (tensor):
+    spatial = tensor.permute(2, 0, 1).numpy()
+    scaler = StandardScaler()
+    spatial = scaler.fit_transform(spatial.reshape(-1, spatial.shape[-1])).reshape(spatial.shape)
+    spatial_mean = np.mean(spatial, axis=1)
+    spatial_median = np.median(spatial, axis=1)
+    spatial = np.concatenate((spatial_mean, spatial_median), axis=1)
+    n_clusters= range(2, 6)
+    best_score = -1
+    best_cluster = None
+    for n in n_clusters:
+        cluster = KMeans(n_clusters=n, init='k-means++', max_iter=300, n_init=10, random_state=0)
+        cluster_labels = cluster.fit_predict(spatial)
+
+        silhouette_avg = silhouette_score(spatial, cluster_labels)
+
+        if silhouette_avg > best_score:
+            best_score = silhouette_avg
+            best_cluster = cluster
+    
+    return best_cluster.fit_predict(spatial)
+
+def split_work_holiday (tensor, day, hour): # tensor of size (4315, 19, 2, 128)
+    workday_tensor = None
+    holiday_tensor = None
+    for i in range(tensor.size(0)):
+        if day <= 4:
+            if workday_tensor is None:
+                workday_tensor = tensor[i].unsqueeze(0)
+                hour, day = move_forward_1h(hour, day)
+            else:
+                workday_tensor = torch.cat((workday_tensor, tensor[i].unsqueeze(0)), dim=0)
+                hour, day = move_forward_1h(hour, day)
+        else:
+            if holiday_tensor is None:
+                holiday_tensor = tensor[i].unsqueeze(0)
+                hour, day = move_forward_1h(hour, day)
+            else:
+                holiday_tensor = torch.cat((holiday_tensor, tensor[i].unsqueeze(0)), dim=0)
+                hour, day = move_forward_1h(hour, day)
+    
+    return workday_tensor, holiday_tensor
+
+def create_temporal_dataloader (x_temporal_tensor, y_temporal_tensor):
+    temporal_scaler = normalize_data(x_temporal_tensor, 'Standard')
+    x_temporal_tensor = temporal_scaler.transform(x_temporal_tensor).to(torch.float)
+    y_temporal_tensor = temporal_scaler.transform(y_temporal_tensor).to(torch.float)
+
+    temporal_dataset = torch.utils.data.TensorDataset(x_temporal_tensor, y_temporal_tensor)
+
+    total_size = len(temporal_dataset)
+    
+    train_size = int(total_size * 0.7)
+    val_size = int(total_size * 0.1)
+    test_size = total_size - train_size - val_size
+
+    train, val, test = torch.utils.data.random_split(temporal_dataset, [train_size, val_size, test_size])
+
+    test_dataloader = torch.utils.data.DataLoader(
+        test,
+        batch_size=64,
+        shuffle=False,
+        drop_last=True
+    )
+
+    temporal_set = {}
+    temporal_set['dataloader'] = test_dataloader
+    temporal_set['scaler'] = temporal_scaler
+
+    return temporal_set
+
+"""
+Below is the main code for data preprocessing
+"""
+
+folder_name = 'NYCBike1'
+
+if not os.path.exists('NYCBike1.h5'):
+    print("NYCBike1.h5 not found. Please download the dataset ")
+    sys.exit(1)
+
+create_folder(folder_name=folder_name)
+
+data = h5py.File('NYCBike1.h5', 'r')
+print(f"Data Keys: {data.keys()}")
+data_tensor = torch.tensor(data['data'])
+print(f"Data Tensor Shape: {data_tensor.shape}")
+tensor = torch.reshape(data_tensor, (data_tensor.shape[0], data_tensor.shape[1], 128))
+
+x_tensor, y_tensor = split_x_y_tensor(tensor)
+
+day=4
+hour=4
+
+time_label = create_time_label(hour, day, x_tensor.size(0))
+
+c = create_traffic_load(y_tensor)
+
+x_train, x_val, x_test = split_train_val_test_sets(x_tensor)
+y_train, y_val, y_test = split_train_val_test_sets(y_tensor)
+time_label_train, time_label_val, time_label_test = split_train_val_test_sets(time_label)
+c_train, c_val, c_test = split_train_val_test_sets(c)
+np.savez(os.path.join(folder_name, 'train.npz'), x=x_train, y=y_train, time_label=time_label_train, c=c_train)
+np.savez(os.path.join(folder_name, 'val.npz'), x=x_val, y=y_val, time_label=time_label_val, c=c_val)
+np.savez(os.path.join(folder_name, 'test.npz'), x=x_test, y=y_test, time_label=time_label_test, c=c_test)
+
+# K-Means for spatial OOD evaluation
+cluster_labels = create_cluster_labels(tensor)
+np.save(os.path.join(folder_name, "cluster_labels.npy"), cluster_labels)
+
+x_workday_tensor, x_holiday_tensor = split_work_holiday(x_tensor, day, hour)
+y_workday_tensor, y_holiday_tensor = split_work_holiday(y_tensor, day, hour)
+
+y_workday_tensor = y_workday_tensor.permute(0, 1, 3, 2)
+y_holiday_tensor = y_holiday_tensor.permute(0, 1, 3, 2)
+x_workday_tensor = x_workday_tensor.permute(0, 1, 3, 2)
+x_holiday_tensor = x_holiday_tensor.permute(0, 1, 3, 2)
+
+total_set = create_temporal_dataloader(x_tensor, y_tensor)
+workday_set = create_temporal_dataloader(x_workday_tensor, y_workday_tensor)
+holiday_set = create_temporal_dataloader(x_holiday_tensor, y_holiday_tensor)
+
+with open(os.path.join(folder_name, "workday_test_dataloader.pkl"), "wb") as f:
+    pickle.dump(workday_set, f)
+print(f"workday_test_dataloader.pkl saved")
+
+with open(os.path.join(folder_name, "holiday_test_dataloader.pkl"), "wb") as f:
+    pickle.dump(holiday_set, f)
+print(f"holiday_test_dataloader.pkl saved")
+
+with open(os.path.join(folder_name, "total_test_dataloader.pkl"), "wb") as f:
+    pickle.dump(total_set, f)
+print(f"total_test_dataloader.pkl saved")
